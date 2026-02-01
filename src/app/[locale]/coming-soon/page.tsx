e8086d04c1437c6985a1a62fdf0071e95915b82c
vapl <valdis.vascenkovs@gmail.com>
2026-02-01 22:15:55 +0200

    chore: remove coming-soon page

--- src/app/[locale]/coming-soon/page.tsx
+++
@@ -1,9 +0,0 @@
-import PreloaderComingSoon from "@/components/PreloaderComingSoon";
-
-export default function ComingSoonPage() {
-  return (
-    <main className="relative flex min-h-screen w-full items-center justify-center bg-black text-center">
-      <PreloaderComingSoon />
-    </main>
-  );
-}
