a2be6742cb538d7136f44b7e1c069e8ebad6a8bd
vapl <valdis.vascenkovs@gmail.com>
2026-02-01 21:34:22 +0200

    feat: created coming-soon page

---
+++ src/app/[locale]/coming-soon/page.tsx
@@ -0,0 +1,9 @@
+import PreloaderComingSoon from "@/components/PreloaderComingSoon";
+
+export default function ComingSoonPage() {
+  return (
+    <main className="relative flex min-h-screen w-full items-center justify-center bg-black text-center">
+      <PreloaderComingSoon />
+    </main>
+  );
+}
